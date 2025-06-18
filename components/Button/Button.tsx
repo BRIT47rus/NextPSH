9c5d6389533d1ac6b1efdb91e6cb23f00884bc00
Vladimir Noutbook <vovirus2@inbox.ru>
2025-06-18 16:39:44 +0300

    button extend DetailsProps

--- components/Button/Button.tsx
+++ components/Button/Button.tsx
@@ -2,13 +2,19 @@ import React from 'react';
 import { IButtonProps } from './Button.props';
 import styles from './Button.module.css';
 import cn from 'classnames';
-export const Button = ({ appearance, children }: IButtonProps) => {
+export const Button = ({
+    appearance,
+    children,
+    className,
+    ...props
+}: IButtonProps) => {
     return (
         <button
-            className={cn(styles.button, {
+            className={cn(styles.button, className, {
                 [styles.primary]: appearance === 'primary',
                 [styles.ghost]: appearance === 'ghost',
             })}
+            {...props}
         >
             {children}
         </button>
