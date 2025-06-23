d36ec9ad4115279a2daa12bf15b557cf74146afe
Vladimir Noutbook <vovirus2@inbox.ru>
2025-06-23 13:22:05 +0300

    footer compleated and grid template

--- app/(site)/components/Footer/Footer.tsx
+++ app/(site)/components/Footer/Footer.tsx
@@ -1,8 +1,23 @@
 import React from 'react';
 import { IFooterProps } from './Footer.props';
+import cn from 'classnames';
+import styles from './Footer.module.css';
+import { format } from 'date-fns';
 
-const Footer = ({ ...props }: IFooterProps) => {
-    return <div {...props}>Footer</div>;
+const Footer = ({ className, ...props }: IFooterProps) => {
+    return (
+        <footer className={cn(className, styles.footer)} {...props}>
+            <div>
+                OwLTop c 2020 - {format(new Date(), 'yyyy')} Все права защищены
+            </div>
+            <a href="#" target="_blank">
+                Пользовательское соглашение
+            </a>
+            <a href="#" target="_blank">
+                Политика конфиденуиальности
+            </a>
+        </footer>
+    );
 };
 
 export default Footer;
--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import Header from './(site)/components/Header/Header';
 import Sidebar from './(site)/components/Sidebar/Sidebar';
 import './globals.css';
 import { Open_Sans } from 'next/font/google';
-
+import styles from './layout.module.css';
 const inter = Open_Sans({ subsets: ['latin'] });
 
 export const metadata = {
@@ -18,10 +18,12 @@ export default function RootLayout({
     return (
         <html lang="ru">
             <body className={inter.className}>
-                <Header />
-                <Sidebar />
-                <div>{children}</div>
-                <Footer />
+                <div className={styles.wrapper}>
+                    <Header className={styles.header} />
+                    <Sidebar className={styles.sidebar} />
+                    <div className={styles.body}>{children}</div>
+                    <Footer className={styles.footer} />
+                </div>
             </body>
         </html>
     );
