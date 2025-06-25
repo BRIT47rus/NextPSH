1ba920d798545d4714921f11c04e3aa71f35e8bf
Vladimir Noutbook <vovirus2@inbox.ru>
2025-06-25 16:01:25 +0300

    changes files

---
+++ app/(site)/products/page44.tsx
@@ -0,0 +1,11 @@
+import { Metadata } from 'next';
+// import { notFound } from 'next/navigation';
+import React from 'react';
+export const metadate: Metadata = {
+    title: 'Продукты',
+};
+const PageProducts = () => {
+    return <div>Продукты</div>;
+};
+
+export default PageProducts;
